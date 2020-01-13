40477b897019d8f2282aca5d711de7c17bd536be
Odohi David <odohi.david@gmail.com>
2020-01-13 21:28:50 +0100

    add some lint corrections and some code refactoring

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
+import pickle
+import json
+import random
+import tensorflow
+import tflearn
+import numpy
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy
-import tflearn
-import tensorflow
-import random
-import json
-import pickle
 
 with open("intents.json") as file:
     data = json.load(file)
 
-#this try block handles the need to continously repeat the block of 
-#below
+# this try block handles the need to continously repeat the block of
+# below
 
-#KNOW THAT if you change anything in your intents file, you must must come here and add x
-#below the try block
+# KNOW THAT if you change anything in your intents file, you must must come here and add x
+# below the try block
 try:
     with open("data.pickle", "rb") as f:
         words, labels, training, output = pickle.load(f)
@@ -26,7 +26,7 @@ except:
     docs_x = []
     docs_y = []
 
-    #check this intent below to see if it has s or not
+    # check this intent below to see if it has s or not
 
     for intent in data["intents"]:
         for pattern in intent["patterns"]:
@@ -45,7 +45,7 @@ except:
 
     training = []
     output = []
-    
+
     out_empty = [0 for _ in range(len(labels))]
 
     for x, doc in enumerate(docs_x):
@@ -71,29 +71,32 @@ except:
     with open("data.pickle", "wb") as f:
         pickle.dump((words, labels, training, output), f)
 
-    #video part 3 begins here
-    #building the neural network model using tensorflow
+    # video part 3 begins here
+    # building the neural network model using tensorflow
 
 
 tensorflow.reset_default_graph()
 
-net = tflearn.input_data(shape = [None, len(training[0])])
+net = tflearn.input_data(shape=[None, len(training[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]), activation = "softmax")
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-#model already exist, so no need to retrain it. use try to catch this
-   
+# model already exist, so no need to retrain it. use try to catch this
+
 try:
+    # This should throw the error first before getting to the next line
+    f = open("model.tflearn")
     model.load("model.tflearn")
-except: 
-    model.fit(training, output, n_epoch= 1000, batch_size = 8, show_metric = True)
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
     model.save("model.tflearn")
 
-      #  words, labels, training, output = pi
+    #  words, labels, training, output = pi
+
 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
@@ -109,8 +112,8 @@ def bag_of_words(s, words):
     return numpy.array(bag)
 
 
-#part 4 video. using the model to start chatting with model
-def chat(): 
+# part 4 video. using the model to start chatting with model
+def chat():
     print("Welcome! You can start chating now( type 'quit' to end)")
     while True:
         inp = input("You: ")
@@ -118,15 +121,15 @@ def chat():
             break
 
         results = model.predict([bag_of_words(inp, words)])[0]
-         #at the point,the output is a probability
+        # at the point,the output is a probability
         results_index = numpy.argmax(results)
-         #at this point, the output is index of the greatest possible result from the probability
+        # at this point, the output is index of the greatest possible result from the probability
         tag = labels[results_index]
-         #at this point, the output is the tag which the index belongs to
-        
+        # at this point, the output is the tag which the index belongs to
+
         if results[results_index] > 0.6:
-            #this if statement determines how high the percentage of the probability must be to accepted
-        
+            # this if statement determines how high the percentage of the probability must be to accepted
+
             for tg in data["intents"]:
                 if tg['tag'] == tag:
                     responses = tg['responses']
@@ -135,7 +138,9 @@ def chat():
         else:
             print("I do not understand that. Could you ask another question?")
 
-chat()
-             
 
-    
+chat()
+
+# download punkt
+# >>> import nltk
+#   >>> nltk.download('punkt')
